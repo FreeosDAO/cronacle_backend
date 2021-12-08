850644549f4584b36ea0bcae08943986e2ec2ef5
Tom McCann <tommccann777@gmail.com>
2021-12-09 12:06:50 +1300

    added timing system

--- cronacle.cpp
+++ cronacle.cpp
@@ -6,7 +6,7 @@
 using namespace eosio;
 using namespace std;
 
-const std::string VERSION = "0.2.0";
+const std::string VERSION = "0.2.11";
 
 class [[eosio::contract("cronacle")]] cronacle : public eosio::contract {
 public:
@@ -21,21 +21,6 @@ public:
     string version_message = "Version = " + VERSION;
 
     check(false, version_message);
-  }
-
-
-  // ACTION: MAINTAIN
-  [[eosio::action]]
-  void maintain(string action, name user) {
-
-    if (action == "clear users") {
-      users_index users_table(get_self(), get_self().value);
-      auto user_iterator = users_table.begin();
-
-      while (user_iterator != users_table.end()) {
-        user_iterator = users_table.erase(user_iterator);
-      }
-    }
   }
 
 
@@ -55,7 +40,9 @@ public:
       });
   } else {
     // modify system record
-    system_table.modify(system_iterator, _self, [&](auto &sys) { sys.init = iterations_start; });
+    system_table.modify(system_iterator, _self, [&](auto &sys) { 
+      sys.init = iterations_start;
+      });
   }
 
 }
@@ -170,6 +157,263 @@ void credit(name user, name to, asset quantity, std::string memo) {
   }
 
 }
+
+
+// ACTION: BID
+[[eosio::action]]
+void bid(name user, uint64_t nft_id, asset bidamount) {
+  require_auth(user);
+
+  // TODO: reject bid if the NFT id is not the one under offer
+
+#ifdef PRODUCTION
+  asset smallest_increment = asset(20000000, CREDIT_CURRENCY_SYMBOL);
+#else
+  asset smallest_increment = asset(200000, CREDIT_CURRENCY_SYMBOL);
+#endif
+
+  bids_index bids_table(get_self(), get_self().value);
+  // count the number of bids
+  uint8_t bids_count = 0;
+  auto bids_itr = bids_table.begin();
+  while (bids_itr != bids_table.end()) {
+    bids_count++;
+    bids_itr++;
+  }
+
+  // find the winning bid
+  auto amt_idx = bids_table.get_index<"byamount"_n>();
+  auto amt_itr = amt_idx.end();
+  amt_itr--;
+
+  asset bid_to_beat = asset(0, CREDIT_CURRENCY_SYMBOL); // zero bid
+  if (amt_itr != amt_idx.cend()) {
+    bid_to_beat = amt_itr->bidamount;
+  }
+
+  /* debugging code: 
+
+  const string bidstr = bidamount.to_string();
+  const string incrstr = smallest_increment.to_string();
+  const string bidtobeatstr = bid_to_beat.to_string();
+  const string diffstr = (bidamount - bid_to_beat).to_string();
+  string result = "";
+  if ((bidamount - bid_to_beat) >= smallest_increment) {
+    result = "success";
+  } else {
+    result = "fail";
+  }
+  const string debugstr = "bid: " + bidstr + " increment: "
+   + incrstr + " bid to beat: " + bidtobeatstr + " difference: " + diffstr
+   + " result: " + result;
+
+  check(false, debugstr);
+
+  end of debugging code */
+  
+
+  const string bid_error = "you must start bidding with, or increase the highest bid by at least " + smallest_increment.to_string();
+  check((bidamount > bid_to_beat) && ((bidamount - bid_to_beat) >= smallest_increment), bid_error);
+
+  // check if we are replacing a previous bid by the same user
+  auto userbid_itr = bids_table.find(user.value);
+  if (userbid_itr != bids_table.end()) {
+    // modify user's bid
+    bids_table.modify(userbid_itr, get_self(), [&](auto &b) {
+      b.bidamount = bidamount;
+    });
+  } else {
+    // at this point we are adding a top bid that from a user who has not bid before
+    // drop the lowest bid from the bids table (if necessary) and emplace the new bid
+
+    // TODO: if there are 3 bids then erase the lowest one
+    if (bids_count == 3) {
+      auto lowest_bid_itr = amt_idx.begin();
+      amt_idx.erase(lowest_bid_itr);
+    }
+
+    // add new top bid
+    bids_table.emplace(
+      get_self(), [&](auto &b) {
+        b.bidtime = current_time_point();
+        b.bidder = user;
+        b.bidamount = bidamount;
+        b.nftid = nft_id;        
+      });
+
+  }
+
+  
+
+
+  // auto amt_itr = amt_idx.find( SECONDARY_KEY_WHICH_YOU_WANT_TO_FIND );
+  // auto bids_iterator = bids_table.begin();
+
+  // get the highest bid
+
+
+
+}
+
+// which auction are we in?
+uint16_t current_auction() {
+
+  uint16_t iteration = 0;
+
+  // get the start of freeos system time
+  system_index system_table(get_self(), get_self().value);
+  auto system_iterator = system_table.begin();
+  check(system_iterator != system_table.end(), "system record is undefined");
+  time_point init = system_iterator->init;
+
+  // how far are we into the current iteration?
+  uint64_t now_secs = current_time_point().sec_since_epoch();
+  uint64_t init_secs = init.sec_since_epoch();
+
+  if (now_secs >= init_secs) {
+    iteration = ((now_secs - init_secs) / AUCTION_LENGTH_SECONDS) + 1;
+  }
+  
+  return iteration;
+}
+
+// ACTION
+void tick() {
+
+  /* are we on a new auction?
+  system_index system_table(get_self(), get_self().value);
+  auto system_iterator = system_table.begin();
+  check(system_iterator != system_table.end(), "system record is undefined");
+
+  uint16_t recorded_auction = system_iterator->auction;
+  uint16_t actual_auction = current_auction();
+
+  if (recorded_auction != actual_auction) {
+    // update the recorded iteration
+    system_table.modify(system_iterator, _self, [&](auto &sys) {
+      sys.auction = actual_auction;
+    });
+
+    // run the new auction service routine
+    trigger_new_auction(actual_auction);
+  }
+  */
+}
+
+
+// ACTION: CLAIM
+[[eosio::action]]
+void claim(name user) {
+  require_auth(user);
+
+  // check that the auction claim time has been reached
+ 
+}
+
+
+// ACTION: AUCTION
+[[eosio::action]]
+void auction(
+    uint32_t number,
+    uint64_t nftid,
+    time_point start,
+    time_point end,
+    name winner,
+    asset bidamount) {
+
+  // TODO: check to see if an auction is already underway
+  time_point now = current_time_point();
+
+  // look for highest auction number - see if it is still active
+  auctions_index auctions_table(get_self(), get_self().value);
+  auto latest_itr = auctions_table.rbegin();
+
+  if (latest_itr == auctions_table.rend()) {
+    check(false, "table is empty");
+  } else {
+    check(false, latest_itr->number);
+  }
+
+  
+
+
+      auctions_table.emplace(
+      get_self(), [&](auto &a) {
+        a.number = number;
+        a.nftid = nftid;
+        a.start = start;
+        a.end = end;
+        a.winner = winner;
+        a.bidamount = bidamount;
+      });
+}
+
+
+void trigger_new_auction(uint16_t auction) {
+
+}
+
+// ACTION: MAINTAIN
+[[eosio::action]]
+void maintain(string action, name user) {
+
+  require_auth(get_self());
+
+  if (action == "set cls") {
+    system_index system_table(get_self(), get_self().value);
+    auto system_iterator = system_table.begin();
+
+    system_table.modify(system_iterator, get_self(), [&](auto &s) {
+      s.cls = asset(1000000, POINT_CURRENCY_SYMBOL);
+    });
+  }
+
+  if (action == "clear users") {
+      users_index users_table(get_self(), get_self().value);
+      auto user_iterator = users_table.begin();
+
+      while (user_iterator != users_table.end()) {
+        user_iterator = users_table.erase(user_iterator);
+      }
+    }
+
+    if (action == "clear system") {
+      system_index system_table(get_self(), get_self().value);
+      auto system_iterator = system_table.begin();
+      system_table.erase(system_iterator);
+    }
+
+    if (action == "set system") {
+      system_index system_table(get_self(), get_self().value);
+      auto system_iterator = system_table.begin();
+
+      if (system_iterator == system_table.end()) {
+        system_table.emplace(get_self(), [&](auto &sys) {
+          // sys.init = time_point("2021-12-05T00:00:00.000");
+          sys.usercount = 1;
+          sys.cls = asset(1000000, POINT_CURRENCY_SYMBOL);          
+        });
+      } else {
+        system_table.modify(system_iterator, get_self(), [&](auto &sys) {
+          // sys.auction = current_auction();
+          sys.usercount = 1;
+          sys.cls = asset(1000000, POINT_CURRENCY_SYMBOL);
+        });
+      }
+    }
+
+    if (action == "clear auctions") {
+      auctions_index auctions_table(get_self(), get_self().value);
+      auto auctions_iterator = auctions_table.begin();
+
+      while (auctions_iterator != auctions_table.end()) {
+        auctions_iterator = auctions_table.erase(auctions_iterator);
+      }
+    }
+
+}
+
+
 
 private:
 
--- cronacle.hpp
+++ cronacle.hpp
@@ -26,15 +26,17 @@ const std::string CREDIT_CURRENCY_CONTRACT = "eosio.token";
 // User contribution to Conditionally Limited Supply
 const asset UCLS = asset(1000000, POINT_CURRENCY_SYMBOL);
 
+// timing system
+const uint32_t AUCTION_LENGTH_SECONDS = 600;    // 10 minutes
+const uint32_t AUCTION_START = 0;
+const uint32_t AUCTION_END = 540;
+
 
 // SYSTEM
 // system table
 struct[[ eosio::table("system"), eosio::contract("cronacle") ]] system_record {
 time_point init;
-uint16_t iteration;
 uint32_t usercount;
-uint32_t claimevents;
-uint32_t participants;
 asset cls;
 
 uint64_t primary_key() const { return 0; } // return a constant to ensure a single-row table
@@ -44,26 +46,60 @@ using system_index = eosio::multi_index<"system"_n, system_record>;
 
 // BTC
 struct[[ eosio::table("btcprice"), eosio::contract("cronacle") ]] btctick {
-time_point  ticktime;
-uint32_t    usdprice;
+    time_point  ticktime;
+    uint32_t    usdprice;
 
-uint64_t primary_key() const { return ticktime.sec_since_epoch(); }
+    uint64_t primary_key() const { return ticktime.sec_since_epoch(); }
 };
 using btcprice_index = eosio::multi_index<"btcprice"_n, btctick>;
 
+
 // USERS
 struct[[ eosio::table("users"), eosio::contract("cronacle") ]] user {
-time_point  time;
-name        proton_account;
-std::string dfinity_principal;
-
+    time_point  time;
+    name        proton_account;
+    std::string dfinity_principal;
 uint64_t primary_key() const { return proton_account.value; }
 };
 using users_index = eosio::multi_index<"users"_n, user>;
 
+
 // CREDITS
 struct[[ eosio::table("credits"), eosio::contract("cronacle") ]] credit {
-asset amount;
-uint64_t primary_key() const { return 0; }  // ensures single record per user
+    asset amount;
+    uint64_t primary_key() const { return 0; }  // ensures single record per user
 };
 using credits_index = eosio::multi_index<"credits"_n, credit>;
+
+
+// BIDS - contains top 3 bids
+struct[[ eosio::table("bids"), eosio::contract("cronacle") ]] userbid {
+    time_point  bidtime;
+    name        bidder;
+    asset       bidamount;
+    uint64_t    nftid;
+
+    uint64_t primary_key() const { return bidder.value; }
+    uint64_t get_secondary() const { return bidamount.amount; }
+};
+using bids_index = eosio::multi_index<"bids"_n, userbid,
+indexed_by<"byamount"_n, const_mem_fun<userbid, uint64_t, &userbid::get_secondary>>>;
+
+
+// AUCTIONS
+struct[[ eosio::table("auctions"), eosio::contract("cronacle") ]] auction {
+    uint32_t    number;
+    uint64_t    nftid;
+    time_point  start;
+    time_point  end;
+    name        winner;
+    asset       bidamount;
+
+    uint64_t primary_key() const { return number; }
+    uint64_t get_secondary() const { return nftid; }
+    uint64_t get_tertiary() const { return winner.value; }
+};
+using auctions_index = eosio::multi_index<"auctions"_n, auction,
+indexed_by<"byndftid"_n, const_mem_fun<auction, uint64_t, &auction::get_secondary>>,
+indexed_by<"bywinner"_n, const_mem_fun<auction, uint64_t, &auction::get_tertiary>>
+>;
